bbbc2188b04bef4b8bd710472940cb452651c6f6
Dominik Scholz <scholzd@in.tum.de>
2016-06-13 15:01:13 +0000

    swapout hashtables (not tested under load)

--- src/hash_map.cpp
+++ src/hash_map.cpp
@@ -5,8 +5,7 @@
 #include <iostream> // for std::endl
 #include <string>
 #include <sparsehash/sparse_hash_map>
-#include <thread>
-#include <unistd.h>
+#include <time.h>
 
 #define unlikely(x)     __builtin_expect(!!(x), 0)
 
@@ -60,8 +59,8 @@ typedef struct sparse_hash_map_cookie_value {
 		#2: unused
 		#3: leftVerified	4
 		#4: rightVerified	8
-		#5: ts1				16
-		#6: ts2				32
+		#5: unused
+		#6: unused
 		#rest: reserved (0)
 	*/
 } sparse_hash_map_cookie_value;
@@ -69,20 +68,49 @@ typedef struct sparse_hash_map_cookie_value {
 using sparse_hash_map_cookie = google::sparse_hash_map<sparse_hash_map_cookie_key, sparse_hash_map_cookie_value*, std::hash<sparse_hash_map_cookie_key> , eq_sparse_hash_map_cookie_key>;
 using namespace std;
 
+typedef struct sparse_hash_maps_cookie {
+	sparse_hash_map_cookie *current;
+	sparse_hash_map_cookie *old;
+	clock_t last_swap;
+} sparse_hash_maps_cookie;
+
 extern "C" {
 	/* Google HashMap Sparsehash */
-	sparse_hash_map_cookie* mg_sparse_hash_map_cookie_create(uint32_t size){
+	double SWAP_INTERVAL = 30;	
+	void mg_sparse_hash_map_cookie_swap(sparse_hash_maps_cookie *maps) {
+		clock_t time = clock();
+		if ( ((double) time - maps->last_swap) > ((double) SWAP_INTERVAL * CLOCKS_PER_SEC) ) {
+			printf("swapping\n");
+			delete maps->old;
+			maps->old = maps->current;
+			maps->current = new sparse_hash_map_cookie(0);
+			maps->last_swap = time;
+		}
+	}
+
+	sparse_hash_maps_cookie* mg_sparse_hash_map_cookie_create(uint32_t size){
+		sparse_hash_maps_cookie *maps = new sparse_hash_maps_cookie;	
+
 		sparse_hash_map_cookie *tmp = new sparse_hash_map_cookie(size);
 		sparse_hash_map_cookie_key k;
 		memset(&k, 0, sizeof(sparse_hash_map_cookie_key));
 		tmp->set_deleted_key(k);
 
-		return tmp;
+		maps->current = tmp;
+		
+		tmp = new sparse_hash_map_cookie(size);
+		memset(&k, 0, sizeof(sparse_hash_map_cookie_key));
+		tmp->set_deleted_key(k);
+
+		maps->old = tmp;
+
+		maps->last_swap = clock();
+
+		return maps;
 	}
 
-
-
 	/* Insert on setLeftVerified
+	 * Always insert into current
 	 * Stores the Ack Number for later calculation of the diff in the diff field
 	 * Sets the leftVerified flag
 	 * If entry already present:
@@ -95,18 +123,19 @@ extern "C" {
 	 * 		if it has changed, something is wrong
 	 * 		hence, we assume the first Ack number to be the correct one and don't update it here
 	 */
-	void mg_sparse_hash_map_cookie_insert(sparse_hash_map_cookie *m, sparse_hash_map_cookie_key *k, uint32_t ack) {
+	void mg_sparse_hash_map_cookie_insert(sparse_hash_maps_cookie *maps, sparse_hash_map_cookie_key *k, uint32_t ack) {
+		auto m = maps->current;
 		auto it = m->find(*k);
 		// not existing yet
         if (it == m->end() ){
  			sparse_hash_map_cookie_value *tmp = new sparse_hash_map_cookie_value;
 			tmp->diff = ack;
-			tmp->flags = 52; // set leftVerified flag 4 + ts flags 16 32
+			tmp->flags = 4; // set leftVerified flag 4
 			(*m)[*k] = tmp;
 			//printf("Entry: %d %d\n", tmp->diff, tmp->flags);
-			return;
 		}
 		//printf("NOT inserted\n");
+		mg_sparse_hash_map_cookie_swap(maps);
     };
 
 	/* Finalize an entry on setRightVerified
@@ -114,23 +143,38 @@ extern "C" {
 	 * Calculate and store diff from seq number and stored ack number
 	 * Set rightVerified flag
 	 */
-	bool mg_sparse_hash_map_cookie_finalize(sparse_hash_map_cookie *m, sparse_hash_map_cookie_key *k, uint32_t seq) {
+	bool mg_sparse_hash_map_cookie_finalize(sparse_hash_maps_cookie *maps, sparse_hash_map_cookie_key *k, uint32_t seq) {
+		auto m = maps->current;
 		auto it = m->find(*k);
 		if (it == m->end() ) {
-			return false;
+			printf("not found in current, checking old\n");
+			m = maps->old;
+			it = m->find(*k);
+			if (it == m->end() ) {
+				printf("also not found here\n");
+				mg_sparse_hash_map_cookie_swap(maps);
+				return false;
+			}
+			// copy and proceed normally
+			printf("found-> copy\n");
+			(*(maps->current))[*k] = (*m)[*k];
 		}
 
  		sparse_hash_map_cookie_value *tmp = (*m)[*k];
 		
 		// Check that flags are correct
 		// Only leftVerified must be set
+		// TODO can we do this without flags..., reduces complexity to single uint32_t
 		if ( unlikely((tmp->flags & 4) != 4) ) {
+			mg_sparse_hash_map_cookie_swap(maps);
 			return false;
 		}
 		
 		tmp->diff = seq - tmp->diff + 1;
-		tmp->flags = tmp->flags | 56; // set rightVerified flag 8 + ts flags 16 32
+		tmp->flags = tmp->flags | 8; // set rightVerified flag 8
 		//printf("Entry: %d %d\n", tmp->diff, tmp->flags);
+		
+		mg_sparse_hash_map_cookie_swap(maps);
 		return true;
 	};
 
@@ -138,10 +182,21 @@ extern "C" {
 	 * If it is verified, update the timestamp bits
 	 * Return the value struct
 	 */	
-	sparse_hash_map_cookie_value* mg_sparse_hash_map_cookie_find_update(sparse_hash_map_cookie *m, sparse_hash_map_cookie_key *k) {
+	sparse_hash_map_cookie_value* mg_sparse_hash_map_cookie_find_update(sparse_hash_maps_cookie *maps, sparse_hash_map_cookie_key *k) {
+		auto m = maps->current;
 		auto it = m->find(*k);
 		if (it == m->end() ) {
-			return 0;
+			printf("not found in current, checking old\n");
+			m = maps->old;
+			it = m->find(*k);
+			if (it == m->end() ) {
+				printf("also not found here\n");
+				mg_sparse_hash_map_cookie_swap(maps);
+				return 0;
+			}
+			// copy and proceed normally
+			printf("found-> copy\n");
+			(*(maps->current))[*k] = (*m)[*k];
 		}
 		
 		sparse_hash_map_cookie_value *tmp = (*m)[*k];
@@ -149,10 +204,11 @@ extern "C" {
 		// Check verified flags (both 4 8 must be set)
 		// TODO maybe even drop this, not necessary really
 		if ((tmp->flags & 12) != 12) {
+			mg_sparse_hash_map_cookie_swap(maps);
 			return 0;
 		}
 
-		tmp->flags = tmp->flags | 48; // update ts flags 16 32
+		mg_sparse_hash_map_cookie_swap(maps);
 		return tmp;
 	};
 	
